cfd7ca7bd54d880efbb184df135ca9ea09149b37
Markus Nordin <belfegorc4@gmail.com>
2026-03-22 06:48:48 +0100

    Configure Sentry and update project structure
    Integrated Sentry monitoring into the BackOffice and API projects. This change includes:
    - Adding Sentry.AspNetCore and UserSecrets to both projects
    - Converting BackOfficeConsole to a Web SDK project to support the Sentry configuration
    - Renaming the DigitalKiosk_Hackathon project to Api.csproj
    - Configuring Sentry middleware with initial debug and tracing settings

--- BackOfficeConsole/Program.cs
+++ BackOfficeConsole/Program.cs
@@ -2,8 +2,27 @@
 
 internal class Program
 {
-    static void Main(string[] args)
+    static async Task Main(string[] args)
     {
-        Console.WriteLine("Hello, World!");
+        var builder = WebApplication.CreateBuilder(args);
+
+        builder.WebHost.UseSentry(o =>
+        {
+            o.Dsn = builder.Configuration["Sentry:Dsn"];
+            // When configuring for the first time, to see what the SDK is doing:
+            o.Debug = true;
+            // Set TracesSampleRate to 1.0 to capture 100%
+            // of transactions for tracing.
+            // We recommend adjusting this value in production
+            o.TracesSampleRate = 1.0;
+            // Enable logs to be sent to Sentry
+            o.EnableLogs = true;
+        });
+
+        var app = builder.Build();
+
+        SentrySdk.CaptureMessage("Hello Sentry"); //Test
+
+        await app.RunAsync();
     }
 }
--- DigitalKiosk_Hackathon/Program.cs
+++ DigitalKiosk_Hackathon/Program.cs
@@ -6,9 +6,21 @@ public class Program
     public static void Main(string[] args)
     {
         var builder = WebApplication.CreateBuilder(args);
+        builder.WebHost.UseSentry(o =>
+        {
+            o.Dsn = builder.Configuration["Sentry:Dsn"];
+            // When configuring for the first time, to see what the SDK is doing:
+            o.Debug = true;
+            // Set TracesSampleRate to 1.0 to capture 100%
+            // of transactions for tracing.
+            // We recommend adjusting this value in production
+            o.TracesSampleRate = 1.0;
+            // Enable logs to be sent to Sentry
+            o.EnableLogs = true;
+        });
+        SentrySdk.CaptureMessage("Hello Sentry"); //Test
 
         // Add services to the container.
-
         builder.Services.AddControllers();
         // Learn more about configuring OpenAPI at https://aka.ms/aspnet/openapi
         builder.Services.AddOpenApi();
