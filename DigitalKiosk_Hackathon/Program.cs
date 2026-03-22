9f5e9b65e56457b1cdfc378255635b75f6e31403
Markus Nordin <belfegorc4@gmail.com>
2026-03-22 08:23:24 +0100

    Adding stuff

--- DigitalKiosk_Hackathon/Program.cs
+++ DigitalKiosk_Hackathon/Program.cs
@@ -32,13 +32,22 @@ public class Program
         builder.Services.AddControllers();
         // Learn more about configuring OpenAPI at https://aka.ms/aspnet/openapi
         builder.Services.AddOpenApi();
+        builder.Services.AddSwaggerGen();
 
         var app = builder.Build();
+
+        app.UseCors("AllowAll"); //Unsafe only for debugging
 
         // Configure the HTTP request pipeline.
         if (app.Environment.IsDevelopment())
         {
             app.MapOpenApi();
+            app.UseSwagger();
+            app.UseSwaggerUI(options =>
+            {
+                options.SwaggerEndpoint("/swagger/v1/swagger.json", "Cafe API v1");
+                options.RoutePrefix = string.Empty;
+            });
         }
 
         app.UseHttpsRedirection();
