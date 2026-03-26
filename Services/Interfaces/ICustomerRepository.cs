f9ebb61bb7886069bcb316c82afbe0df762cdeb0
farzad-198 <farzadjafarzadehtey@gmail.com>
2026-03-26 11:13:24 +0100

    fixa metoder ICustomerRepository

--- Services/Interfaces/ICustomerRepository.cs
+++ Services/Interfaces/ICustomerRepository.cs
@@ -10,6 +10,6 @@ public interface ICustomerRepository
     Task UpdateNameAsync(Customer customer);
     Task UpdateIdentityNumberAsync(Customer customer);
     Task DeleteAsync(Guid id);
-    Task<Customer?> GetByPersonalIdentityNumberAsync(Customer);
+    Task<Customer?> GetByPersonalIdentityNumberAsync(string personalIdentityNumber);
     
 }
